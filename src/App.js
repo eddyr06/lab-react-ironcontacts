d3a448d46d3de3207afcc171f71c885bec264f2d
Eddy Ruiz <eddy@eddys-mbp.lan>
2021-11-17 16:45:47 -0500

    iteration 3

--- src/App.js
+++ src/App.js
@@ -10,12 +10,21 @@ for (let i = 0; i < 5; i++){
   contact.push(contacts[i])
 }
 
+const filterItems = (arr, query) => {
+  return arr.filter(el => el.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+}
+
+filterItems(contact, 'jo') // ['apple', 'grapes']
+
 
 function App() {
   const [contactArr, setContactArr] = useState(contact)
   // const [contactArr, setContactArr] = React.useState(contact)
   let array1 = contactArr
   let array2 = contacts[Math.floor(Math.random() * contacts.length)]
+  let array3 = array1.concat(array2)
+  // ... is to clone an array that is a hook.
+  let newArr = [...contactArr]
 
   return (
     <div className="App">
@@ -25,16 +34,43 @@ function App() {
             <th>IronContacts</th>
           </tr>
 
-          <button
+          <tr>
+            <th><button
+              onClick={(e) => setContactArr(
+                array3)}
+            >Add Random Contact
+            </button></th>
+            <th><button
+              onClick={(e) => setContactArr(
+                newArr.sort(function (a, b) {
+                  let nameA = a.name.toUpperCase()
+                  let nameB = b.name.toUpperCase()
+                  if (nameA < nameB) {
+                    return -1
+                  } if (nameA > nameB) {
+                    return 1
+                  }
+                  return 0
+                })
+              )
+              }
+            >Sort by name</button></th>
+          <th><button
             onClick={(e) => setContactArr(
-              array1.concat(array2))}
-          >Add Random Contact
-          </button>
-
+            // contact.sort(function (a, b) {
+//   return b.popularity - a.popularity
+// });
+              newArr.sort(function (a, b) {
+                return b.popularity - a.popularity
+              })
+            )}
+            >Sort by popularity</button></th>
+          </tr>
           <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>{contactArr.map((contact) => {
@@ -42,9 +78,17 @@ function App() {
             <tr>
                   <td><img src={contact.pictureUrl} style={{ height: 100, width: 70 }} alt={contact.name}/></td>
                   <td>{contact.name }</td>
-                  <td>{contact.popularity}</td>
+                  <td>{contact.popularity.toFixed(2)}</td>
+                  <td><button
+                      onClick={(e) => setContactArr(
+                      )}
+                      
+                      >Delete</button>
+                  </td>
             </tr>)
-        }) }</tbody>
+        })}
+        
+        </tbody>
       </table>
     </div>
   );
