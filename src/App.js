781c5e3332d4e1b224e82c8dd61a16b7a35082a7
Eddy Ruiz <eddy@eddys-mbp.lan>
2021-11-18 09:12:25 -0500

    itiration 4

--- src/App.js
+++ src/App.js
@@ -10,21 +10,48 @@ for (let i = 0; i < 5; i++){
   contact.push(contacts[i])
 }
 
-const filterItems = (arr, query) => {
-  return arr.filter(el => el.toLowerCase().indexOf(query.toLowerCase()) !== -1)
-}
-
-filterItems(contact, 'jo') // ['apple', 'grapes']
 
 
 function App() {
   const [contactArr, setContactArr] = useState(contact)
-  // const [contactArr, setContactArr] = React.useState(contact)
-  let array1 = contactArr
-  let array2 = contacts[Math.floor(Math.random() * contacts.length)]
-  let array3 = array1.concat(array2)
   // ... is to clone an array that is a hook.
   let newArr = [...contactArr]
+  
+  const setRandomContact = () => {
+    let random = Math.floor(Math.random() * contacts.length)
+    setContactArr(contactArr.concat(contacts[random]))
+  }
+
+  const sortName = () => {
+    let orderedArr = newArr.sort((a, b) => {
+                  let nameA = a.name.toUpperCase()
+                  let nameB = b.name.toUpperCase()
+                  if (nameA < nameB) {
+                    return -1
+                  } if (nameA > nameB) {
+                    return 1
+                  }
+                  return 0
+    })
+    setContactArr(orderedArr);
+  }
+  
+  const sortPopularity = () => {
+      let orderedArr = newArr.sort((a, b) => {
+                return b.popularity - a.popularity
+                })
+    setContactArr(orderedArr)
+  }
+
+
+  const deleteLine = (name) => {
+    newArr = newArr.filter((celeb) => {
+      return celeb.name !== name;
+    })
+
+    setContactArr(newArr)
+  }
+
 
   return (
     <div className="App">
@@ -35,36 +62,9 @@ function App() {
           </tr>
 
           <tr>
-            <th><button
-              onClick={(e) => setContactArr(
-                array3)}
-            >Add Random Contact
-            </button></th>
-            <th><button
-              onClick={(e) => setContactArr(
-                newArr.sort(function (a, b) {
-                  let nameA = a.name.toUpperCase()
-                  let nameB = b.name.toUpperCase()
-                  if (nameA < nameB) {
-                    return -1
-                  } if (nameA > nameB) {
-                    return 1
-                  }
-                  return 0
-                })
-              )
-              }
-            >Sort by name</button></th>
-          <th><button
-            onClick={(e) => setContactArr(
-            // contact.sort(function (a, b) {
-//   return b.popularity - a.popularity
-// });
-              newArr.sort(function (a, b) {
-                return b.popularity - a.popularity
-              })
-            )}
-            >Sort by popularity</button></th>
+            <th><button onClick={setRandomContact}>Add Random Contact</button></th>
+            <th><button onClick={sortName}>Sort by name</button></th>
+            <th><button onClick={sortPopularity}>Sort by popularity</button></th>
           </tr>
           <tr>
             <th>Picture</th>
@@ -79,12 +79,7 @@ function App() {
                   <td><img src={contact.pictureUrl} style={{ height: 100, width: 70 }} alt={contact.name}/></td>
                   <td>{contact.name }</td>
                   <td>{contact.popularity.toFixed(2)}</td>
-                  <td><button
-                      onClick={(e) => setContactArr(
-                      )}
-                      
-                      >Delete</button>
-                  </td>
+                  <td><button onClick={() => deleteLine(contact.name)}>Delete</button></td>
             </tr>)
         })}
         
